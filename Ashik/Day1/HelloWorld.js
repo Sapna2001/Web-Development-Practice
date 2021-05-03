37a559098b864715c4fa9af390202fb83fabf520
Ashik-Jose <ashiksholly@gmail.com>
2021-05-03 19:42:09 +0530

    Day1HelloWorld

---
+++ Ashik/Day1/HelloWorld.js
@@ -0,0 +1,18 @@
+// Day 1 Hello World
+
+var parametersVariable;
+
+function myFunction(parametersVariable)
+{
+    console.log("Hello,World!");
+    console.log(parametersVariable);
+}
+
+void main()
+ {
+
+    var parametersVariable = readLine();
+
+    myFunction(parametersVariable);
+}
+
