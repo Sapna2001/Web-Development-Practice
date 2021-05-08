7a3f3f50be9990994a72877bbb790e4a953133e3
Aswin A Ashok <aswinaashok77@gmail.com>
2021-05-08 19:46:13 +0530

    Relevant message

---
+++ Aswin/Day 1/Datatypes.js
@@ -0,0 +1,22 @@
+var full_name = "Aswin A Ashok";
+
+var course_name = "React JS Bootcamp";//string
+
+var course_name = false;//boolean
+
+var logged_count = 17;//number
+
+console.log(full_name);
+
+console.log(logged_count);
+
+console.log("logged_count");
+
+console.log(9);
+
+console.log(`
+user is : ${full_name}
+
+
+
+`);
