089a21141af1f62dd4621ce469b44c5994ab26cd
Aswin A Ashok <aswinaashok77@gmail.com>
2021-05-09 21:47:07 +0530

    Relevant message

---
+++ Aswin/Day 2/Let_and_Const.js
@@ -0,0 +1,12 @@
+const PI = 3.14;
+let r = 7;
+
+   var area = PI*r*r;
+
+
+ 
+    var perimeter =  2*PI*r;
+ 
+
+console.log("Area of circle is",area);
+console.log("Perimeter of circle is",perimeter);
